34f8696cd6c46a01bec480c70da7b0a8176a351b
Akash Vacher <akash.phi@gmail.com>
2019-12-03 20:50:50 +0530

    Added day2 solutions

---
+++ day2/day2.1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+def compute(a):
+	'''
+	Accept an intcode program as a list of integers
+	and return the final output of running the program
+	'''
+	it = iter(a)
+	while True:
+		op = next(it)
+
+		# Invalid opcode check
+		if op not in (1, 2, 99):
+			raise ValueError
+		
+		# Halt opcode
+		if op == 99:
+			break
+
+		x, y, z = next(it), next(it), next(it)
+		if op == 1:
+			a[z] = a[x] + a[y]
+		else:
+			a[z] = a[x] * a[y]
+
+	return a[0]
+
+
+def load_input():
+	return list(map(int, open('in.txt').read().split(',')))
+
+a = load_input()
+a[1], a[2] = 12, 2
+
+print(compute(a))
---
+++ day2/day2.2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+def compute(a):
+	'''
+	Accept an intcode program as a list of integers
+	and return the final output of running the program
+	'''
+	it = iter(a)
+	while True:
+		op = next(it)
+
+		# Invalid opcode check
+		if op not in (1, 2, 99):
+			raise ValueError
+		
+		# Halt opcode
+		if op == 99:
+			break
+
+		x, y, z = next(it), next(it), next(it)
+		if op == 1:
+			a[z] = a[x] + a[y]
+		else:
+			a[z] = a[x] * a[y]
+
+	return a[0]
+
+
+def load_input():
+	return list(map(int, open('in.txt').read().split(',')))
+
+a = load_input()
+done = False
+for noun in range(100):
+	for verb in range(100):
+		# print(f"Checking {noun} {verb}")
+		a[1], a[2] = noun, verb
+		try:
+			# Need to pass a copy of list otherwise 
+			# the original list will get mutated
+			if compute(a[:]) == 19690720:
+				print(100 * noun + verb)
+				done = True
+		except:
+			pass
+		if done:
+			break
+	if done:
+		break
