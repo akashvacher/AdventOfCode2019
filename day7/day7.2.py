0071b3caca7b88a750a7dd4232dd5c2340249374
Akash Vacher <akash.phi@gmail.com>
2019-12-12 01:54:15 +0530

    Added day7 part2 solution

--- day7/day7.2.py
+++ day7/day7.2.py
@@ -1,45 +1,57 @@
 #!/usr/bin/env python
 import logging
-from itertools import permutations
+from itertools import permutations, cycle
+from collections import deque
 
 logging.basicConfig(level=logging.WARN)
-
 op_to_param = {1: 3, 2: 3, 3: 1, 4: 1, 5: 2, 6: 2, 7: 3, 8: 3, 99: 0}
 
 
 class Computer:
-    def __init__(self):
+    def __init__(self, a, inp):
         self.ip = 0
+        self.a = a
+        self.inp = deque(inp)
+        self.out = deque([])
 
-    def get_item(self, a):
-        i = a[self.ip]
+    def get_input(self):
+        return self.inp.popleft()
+
+    def append_input(self, x):
+        self.inp.append(x)
+
+    def append_output(self, x):
+        self.out.append(x)
+
+    def get_item(self):
+        i = self.a[self.ip]
         self.ip += 1
         return i
 
-    def op_exec(self, op, params, mode, a, IN, OUT, address=None):
+    def op_exec(self, op, params, address=None):
         if op == 1:
-            a[address] = sum(params)
+            self.a[address] = sum(params)
             logging.info(
-                f"1 - Adding {params} and storing solution ({a[address]}) at index {address}"
+                f"1 - Adding {params} and storing solution ({self.a[address]}) at index {address}"
             )
 
         if op == 2:
             ans = 1
             for i in params:
                 ans *= i
-            a[address] = ans
+            self.a[address] = ans
             logging.info(
-                f"2 - Multiplying {params} and storing solution ({a[address]}) at index {address}"
+                f"2 - Multiplying {params} and storing solution ({self.a[address]}) at index {address}"
             )
 
         if op == 3:
-            inp = next(IN)
-            a[address] = inp
+            inp = self.get_input()
+            self.a[address] = inp
             logging.info(f"3 - Storing input {inp} at index {address}")
 
         if op == 4:
-            OUT.append(params[0])
-            logging.info(f"4 - Producing output {OUT[-1]}")
+            self.append_output(params[0])
+            logging.info(f"4 - Producing output {params[0]}")
 
         if op == 5:
             logging.info(f"5 - Checking if {params[0]} is not equal to zero")
@@ -55,25 +67,22 @@ class Computer:
 
         if op == 7:
             logging.info(f"7 - Checking if {params[0]} is less that {params[1]}")
-            a[address] = 1 if (params[0] < params[1]) else 0
+            self.a[address] = 1 if (params[0] < params[1]) else 0
 
         if op == 8:
             logging.info(f"8 - Checking if {params[0]} is equal to {params[1]}")
-            a[address] = 1 if (params[0] == params[1]) else 0
+            self.a[address] = 1 if (params[0] == params[1]) else 0
 
-    def compute(self, a, inp):
+    def compute(self):
         """
-        Accept an intcode program and an input stream (as a lists of integers)
-        and return the final output stream after running the program
+        yield the output item after running the program
         """
-        IN = iter(inp)
-        OUT = []
-        logging.info(f"Array state before we begin:\n{a}")
+        logging.info(f"Array state before we begin:\n{self.a}")
         while True:
-            instruction = self.get_item(a)
+            instruction = self.get_item()
             if instruction < 0:
                 raise ValueError(
-                    f"Invalid instruction encountered: {instruction}\nInstruction pointer: {self.ip}\nProgram state: {a}"
+                    f"Invalid instruction encountered: {instruction}\nInstruction pointer: {self.ip}\nProgram state: {self.a}"
                 )
 
             op = instruction % 100
@@ -82,16 +91,16 @@ class Computer:
             # Invalid opcode check
             if op not in op_to_param.keys():
                 raise ValueError(
-                    f"Invalid instruction encountered: {instruction}\nInstruction pointer: {self.ip}\nProgram state: {a}"
+                    f"Invalid instruction encountered: {instruction}\nInstruction pointer: {self.ip}\nProgram state: {self.a}"
                 )
 
             # Halt opcode
             if op == 99:
                 logging.info("Halting now")
-                break
+                yield None
 
             param_count = op_to_param[op]
-            params = [self.get_item(a) for i in range(param_count)]
+            params = [self.get_item() for i in range(param_count)]
             address = None
             if op in (1, 2, 3, 7, 8):
                 # Isolate last param as it's the address
@@ -102,11 +111,12 @@ class Computer:
             # in "position" mode or "immediate" mode.
             for i in range(param_count):
                 if mode % 10 == 0:
-                    params[i] = a[params[i]]
+                    params[i] = self.a[params[i]]
                 mode //= 10
 
-            self.op_exec(op, params, mode, a, IN, OUT, address)
-        return OUT
+            self.op_exec(op, params, address)
+            if len(self.out) > 0:
+                yield self.out.popleft()
 
 
 def load_input():
@@ -114,15 +124,26 @@ def load_input():
 
 
 def get_thrust(phases, a):
-    signal = 0
+    amps = []
     for phase in phases:
-        signal = Computer().compute(a[:], inp=[phase, signal])[0]
-    return signal
+        amps.append(Computer(a[:], inp=[phase]))
+
+    signal, last_signal, over = 0, 0, 0
+    for i, phase in cycle(enumerate(phases)):
+        if signal is not None:
+            amps[i].append_input(signal)
+        signal = next(amps[i].compute())
+        last_signal = signal if signal is not None else last_signal
+        if signal is None:
+            over += 1
+        if over == 5:
+            break
+    return last_signal
 
 
 def get_max_thrust(a):
     max_thrust = 0
-    for phases in permutations(range(5)):
+    for phases in permutations(range(5, 10)):
         logging.info(f"Trying: {phases}")
         max_thrust = max(max_thrust, get_thrust(phases, a))
     return max_thrust
@@ -130,4 +151,3 @@ def get_max_thrust(a):
 
 a = load_input()
 print(get_max_thrust(a))
-
